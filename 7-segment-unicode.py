bede647f5568e35afba69ae946a0eba43c7fbe38
William Wold <wm@wmww.sh>
2018-12-01 02:05:10 -0500

    Fixes

--- 7-segment-unicode.py
+++ 7-segment-unicode.py
@@ -76,11 +76,11 @@ def get_braille(left_side):
 
 def encode_digit(num):
     data = digits[num]
-    left = get_braille([data[i] for i in [0, 1, 4, 6]])
-    right = get_braille([data[i] for i in [0, 2, 5, 6]])
+    left = get_braille([data[0] or data[1], data[1], data[4], data[4] or data[6]])
+    right = get_braille([data[0] or data[2], data[2], data[5], data[5] or data[6]])
     out = left
     if data[0]:
-        out += chr(0x0340)
+        out += chr(0x0304)
     if data[3]:
         out += chr(0x0335)
     if data[6]:
@@ -95,6 +95,10 @@ if __name__ == '__main__':
         for c in arg:
             if ord(c) >= 48 and ord(c) < 58:
                 out += encode_digit(int(c))
+            elif c == '.':
+                out += get_braille([0, 0, 0, 1])
+            elif c == ':':
+                out += get_braille([0, 1, 1, 0])
             else:
                 out += c
         out += ' '
